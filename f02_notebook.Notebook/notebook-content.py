cf023d485749c96dfbba3499aa0e4e5eb3781556
Marco Aurélio <marco.conviccao@gmail.com>
2025-10-27 20:43:49 -0300

    Committing 2 items from workspace 8bafa015-7a97-4024-8b83-607a4fcc7c07

---
+++ f02_notebook.Notebook/notebook-content.py
@@ -0,0 +1,103 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "7d05d9ca-4a6e-4eaa-8ed2-a13f9e1f8fca",
+# META       "default_lakehouse_name": "f02_lakehouse",
+# META       "default_lakehouse_workspace_id": "8bafa015-7a97-4024-8b83-607a4fcc7c07",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "7d05d9ca-4a6e-4eaa-8ed2-a13f9e1f8fca"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+from pyspark.sql.types import *
+from pyspark.sql.functions import *
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+orderSchema = StructType([
+    StructField("SalesOrderNumber", StringType()),
+    StructField("SalesOrderLineNumber", IntegerType()),
+    StructField("OrderDate", DateType()),
+    StructField("CustomerName", StringType()),
+    StructField("Email", StringType()),
+    StructField("Item", StringType()),
+    StructField("Quantity", IntegerType()),
+    StructField("UnitPrice", FloatType()),
+    StructField("Tax", FloatType())
+])
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+df = spark.read.format("csv").schema(orderSchema).load("Files/orders/*.csv")
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+transformed_df = df.withColumn("Year", year(col("OrderDate"))).withColumn("Month", month(col("OrderDate")))
+transformed_df = transformed_df.withColumn("DayOfWeek", dayofweek(col("OrderDate"))).withColumn("DayOfMonth", dayofmonth(col("OrderDate")))
+transformed_df = transformed_df.withColumn("FirstName", split(col("CustomerName"), " ").getItem(0)).withColumn("LastName", split(col("CustomerName"), " ").getItem(1))
+#transformed_df = transformed_df["SalesOrderNumber", "SalesOrderLineNumber", "OrderDate", "Year", "Month", "FirstName", "LastName", "Email", "Item", "Quantity", "UnitPrice", "Tax"]
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+transformed_df.write.mode("overwrite").parquet("Files/transformed_data/orders")
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+df.write.format("delta").mode("overwrite").saveAsTable("salesorders")
+
+spark.sql("DESCRIBE EXTENDED salesorders").show(truncate=False)
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
